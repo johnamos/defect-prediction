b49007f900c8f16540dfd103461ee97c8bd17f48
unknown <jamos@jamos12408.imany.com>
2013-01-02 21:26:55 -0800

    Fixed problem with source paths

--- src/main/java/com/revitasinc/sonar/dp/batch/DefectPredictionDecorator.java
+++ src/main/java/com/revitasinc/sonar/dp/batch/DefectPredictionDecorator.java
@@ -1,7 +1,7 @@
 package com.revitasinc.sonar.dp.batch;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.File;
+
 import org.sonar.api.batch.Decorator;
 import org.sonar.api.batch.DecoratorContext;
 import org.sonar.api.measures.MeasureUtils;
@@ -11,9 +11,12 @@ import org.sonar.api.resources.Scopes;
 
 import com.revitasinc.sonar.dp.DefectPredictionMetrics;
 
+/**
+ * Sets the score on each resource and sums the scores at higher levels.
+ * 
+ * @author John Amos (jamos@revitasinc.com)
+ */
 public class DefectPredictionDecorator implements Decorator {
-  private static Logger logger = LoggerFactory.getLogger(DefectPredictionDecorator.class);
-
   public boolean shouldExecuteOnProject(Project project) {
     return true;
   }
@@ -25,11 +28,10 @@ public class DefectPredictionDecorator implements Decorator {
     // -> modules -> project
     if (Scopes.isFile(resource) && DefectPredictionSensor.getScoreMap() != null) {
       // Add a measure to the current file
-      Double val = DefectPredictionSensor.getScoreMap().get(resource.getKey());
+      Double val = findValueForResource(resource, context);
       if (val != null) {
         context.saveMeasure(DefectPredictionMetrics.DEFECT_PREDICTION, val);
       }
-      logger.debug("resource.getKey(): " + resource.getKey());
     }
     else if (Scopes.isHigherThan(resource, Scopes.FILE)) {
       // sum values at higher levels
@@ -41,5 +43,33 @@ public class DefectPredictionDecorator implements Decorator {
   @Override
   public String toString() {
     return "DefectPredictionDecorator";
+  }
+
+  /**
+   * Tries several paths for the supplied resource to get the score from the Map
+   * built from the SCM log file.
+   * 
+   * @param resource
+   * @param context
+   * @return
+   */
+  @SuppressWarnings("rawtypes")
+  private Double findValueForResource(Resource resource, DecoratorContext context) {
+    String filename = resource.getKey().replace('.', '/') + ".java";
+    Double val = DefectPredictionSensor.getScoreMap().get(filename);
+    if (val == null) {
+      String basepath = context.getProject().getFileSystem().getBasedir().getPath();
+      for (File dir : context.getProject().getFileSystem().getSourceDirs()) {
+        String dirpath = dir.getPath();
+        if (dirpath.startsWith(basepath) && dirpath.length() > basepath.length()) {
+          String prepath = dirpath.substring(basepath.length() + 1).replace('\\', '/');
+          val = DefectPredictionSensor.getScoreMap().get(prepath + "/" + filename);
+          if (val != null) {
+            break;
+          }
+        }
+      }
+    }
+    return val;
   }
 }
